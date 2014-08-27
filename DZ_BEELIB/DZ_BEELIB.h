bc2cbb7793846730dc07c2dcd2bc1dbf1df5ba78
Natoto <787038442@qq.com>
2014-08-27 11:20:56 +0800

    v 0.0.1
    bee framework for dz project

---
+++ DZ_BEELIB/DZ_BEELIB.h
@@ -0,0 +1,13 @@
+//
+//  DZ_BEELIB.h
+//  DZ_BEELIB
+//
+//  Created by Nonato on 14-8-27.
+//  Copyright (c) 2014年 Nonato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DZ_BEELIB : NSObject
+
+@end
---
+++ DZ_BEELIB/DZ_BEELIB.m
@@ -0,0 +1,13 @@
+//
+//  DZ_BEELIB.m
+//  DZ_BEELIB
+//
+//  Created by Nonato on 14-8-27.
+//  Copyright (c) 2014年 Nonato. All rights reserved.
+//
+
+#import "DZ_BEELIB.h"
+
+@implementation DZ_BEELIB
+
+@end
